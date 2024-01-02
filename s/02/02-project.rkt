942b000a1dfb44feda27ff30768a050fdfe1109d
jovyan <jovanprodanov123@gmail.com>
2024-01-02 18:26:53 +0100

    add data types

--- s/02/02-project.rkt
+++ s/02/02-project.rkt
@@ -1,1 +1,20 @@
 #lang racket
+
+(struct true () #:transparent)
+(struct false () #:transparent)
+(struct int (n) #:transparent)
+(struct exception (exn) #:transparent)
+
+(define-syntax-rule (.. e1 e2)
+  (cons e1 e2))
+(define empty '())
+
+(define (fri expr env)
+  (cond
+    [(true? expr) #t]
+    [(false? expr) #f]
+    [(int? expr) expr]
+    [(exception? expr) (raise (exception-exn expr))]
+    [else (error "Expression not found")]
+    )
+  )
